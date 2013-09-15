b6e6fb24ebf3d84eee8a3e962540d82f3b10cccc
Raymond Chandra <ray_flare@yahoo.com>
2013-09-16 01:02:04 +0700

    product + bisnis report

--- relmon/Controllers/FrontEnd/DataBisnisApController.cs
+++ relmon/Controllers/FrontEnd/DataBisnisApController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using relmon.Utilities;
 using iTextSharp.text.pdf;
 using relmon.Models;
+using Telerik.Web.Mvc.UI;
 
 namespace relmon.Controllers.FrontEnd
 {
@@ -51,6 +52,130 @@ namespace relmon.Controllers.FrontEnd
         {
             ViewBag.id = id;
             return PartialView();
+        }
+
+
+
+        public ActionResult Product(int id,string produk,int tahun, int fromYear, int toYear, string fromMonth, string toMonth)
+        {
+            ViewBag.id = id;
+            ViewBag.produk = produk;
+            ViewBag.tahun = tahun;
+            ViewBag.fromYear = fromYear;
+            ViewBag.toYear = toYear;
+            ViewBag.fromMonth = fromMonth;
+            ViewBag.toMonth = toMonth;
+            ViewBag.fromMonthNum = changeNumber(fromMonth);
+            ViewBag.toMonthNum = changeNumber(toMonth);
+            return PartialView();
+        }
+
+         public int changeNumber(string a) {
+            if (a == "Januari") {
+                return 1;
+            } else if (a == "Februari") {
+                return 2;
+            } else if (a == "Maret") {
+                return 3;
+            } else if (a == "April") {
+                return 4;
+            } else if (a == "Mei") {
+                return 5;
+            } else if (a == "Juni") {
+                return 6;
+            } else if (a == "Juli") {
+                return 7;
+            } else if (a == "Agustus") {
+                return 8;
+            } else if (a == "September") {
+                return 9;
+            } else if (a == "Oktober") {
+                return 10;
+            } else if (a == "November") {
+                return 11;
+            } else if (a == "Desember") {
+                return 12;
+            } else {
+                return -1;
+            }
+        }
+        public ActionResult ProductNavigator(int id)
+        {
+            ViewBag.id = id;
+            var listResultTemp = (from x in db.bisnis_product
+                                  where x.company_id == id
+                                  select x.product).Distinct().ToList();
+            ViewData["products"] = listResultTemp;
+            return PartialView();
+        }
+        public ActionResult _ProductData(int id, string produk,int tahun)
+        {
+            var prod = produk.Replace('_', ' ');
+            var listResultTemp = (from x in db.bisnis_product
+                         where x.company_id == id && x.product == prod && x.tahun == tahun
+                         select x).ToList();
+            List<object> listResult = new List<object>();
+            foreach (var b in listResultTemp)
+            {
+                listResult.Add(new
+                {
+                    company_id = b.company_id,
+                    product = b.product,
+                    id = b.id,
+                    tahun = b.tahun,
+                    januari = b.januari,
+                    februari = b.februari,
+                    maret = b.maret,
+                    april = b.april,
+                    mei = b.mei,
+                    juni = b.juni,
+                    juli = b.juli,
+                    agustus = b.agustus,
+                    september = b.september,
+                    oktober = b.oktober,
+                    november = b.november,
+                    desember = b.desember
+                });
+            }
+
+            return Json(listResult);
+            //return Json(SalesDataBuilder.GetCollection());
+
+        }
+
+        public ActionResult _ProductData2(int id, string produk, int tahun,int tahunAkhir)
+        {
+            var prod = produk.Replace('_', ' ');
+            var listResultTemp = (from x in db.bisnis_product
+                                  where x.company_id == id && x.product == prod && x.tahun >= tahun && x.tahun <= tahunAkhir
+                                  select x).ToList();
+            List<object> listResult = new List<object>();
+            foreach (var b in listResultTemp)
+            {
+                listResult.Add(new
+                {
+                    company_id = b.company_id,
+                    product = b.product,
+                    id = b.id,
+                    tahun = b.tahun,
+                    januari = b.januari,
+                    februari = b.februari,
+                    maret = b.maret,
+                    april = b.april,
+                    mei = b.mei,
+                    juni = b.juni,
+                    juli = b.juli,
+                    agustus = b.agustus,
+                    september = b.september,
+                    oktober = b.oktober,
+                    november = b.november,
+                    desember = b.desember
+                });
+            }
+
+            return Json(listResult);
+            //return Json(SalesDataBuilder.GetCollection());
+
         }
     }
 }
