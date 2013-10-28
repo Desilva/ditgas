def5fbc074fb9dc395219bb30327d8e212cb4d65
Raymond Chandra <ray_flare@yahoo.com>
2013-10-28 17:05:12 +0700

    webservicecontainer

---
+++ relmon/Models/WebServiceContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace relmon.Models
+{
+    public class WebServiceContainer
+    {
+
+        public string Directoratecode { get; set; }
+        public string Directoratedesc { get; set; }
+        public string Departementcode { get; set; }
+        public string Departementdesc { get; set; }
+        public string Divisioncode { get; set; }
+        public string Divisiondesc { get; set; }
+        public string Functioncode { get; set; }
+        public string Functiondesc { get; set; }
+        public string Sectioncode { get; set; }
+        public string Sectiondesc { get; set; }
+        public string Groupcode { get; set; }
+        public string Groupdesc { get; set; }
+        public string EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ParentPositionID { get; set; }
+        public string EmployeeStatus { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BirthDate { get; set; }
+        public string StartWorkDate { get; set; }
+        public string EmployeeTitle { get; set; }
+        public string Layer { get; set; }
+        public string PositionID { get; set; }
+        public string GolJabatan { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+    }
+}
