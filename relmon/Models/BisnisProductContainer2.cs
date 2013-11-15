d700954f6ff98cd72749df991d9f82a41d95de45
Raymond Chandra <ray_flare@yahoo.com>
2013-11-15 08:04:45 +0700

    container 2

---
+++ relmon/Models/BisnisProductContainer2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace relmon.Models
+{
+    public class BisnisProductContainer2
+    {
+        public string product { get; set; }
+        public string month { get; set; }
+        public int sum { get; set; }
+    }
+}
